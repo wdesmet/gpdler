090313d3634491b42f11140a5b72f137c7e63c28
Wim De Smet <Wim.DeSmet@UGent.be>
2013-03-19 14:37:03 +0100

    Fixed another bad import

--- src/test/java/net/straininfo2/grs/idloader/bioproject/xmlparsing/DomainConversionTest.java
+++ src/test/java/net/straininfo2/grs/idloader/bioproject/xmlparsing/DomainConversionTest.java
@@ -8,8 +8,8 @@ import net.straininfo2.grs.idloader.bioproject.domain.Publication;
 import org.junit.Before;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static net.straininfo2.grs.idloader.bioproject.domain.Archive.NCBI;
 
 /**
